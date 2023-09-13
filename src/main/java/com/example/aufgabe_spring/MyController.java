318135434bc160c8bb6f727de1f11de67b0a89c3
WeiMing <110397919+WeiMingTay@users.noreply.github.com>
2023-09-13 16:12:15 +0200

    PathVariable erstellt

--- src/main/java/com/example/aufgabe_spring/MyController.java
+++ src/main/java/com/example/aufgabe_spring/MyController.java
@@ -1,14 +1,18 @@
 package com.example.aufgabe_spring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class MyController {
-@GetMapping
-    public String getHello(){
+
+    @GetMapping
+    public String getHello() {
         return "Hello, World";
+    }
+
+    @PostMapping("/{name}")
+    public String postHello(@PathVariable String name) {
+        return "Hello, "+name;
     }
 }
